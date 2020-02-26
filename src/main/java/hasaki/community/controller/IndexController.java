89e65d61837291932be6bd10502398274f00fbd7
QHasakiQ <466423955@qq.com>
2020-02-26 16:38:07 +0800

    extract github config from code to properties

--- src/main/java/hasaki/community/controller/IndexController.java
+++ src/main/java/hasaki/community/controller/IndexController.java
@@ -1,5 +1,7 @@
 package hasaki.community.controller;
 
+import hasaki.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import java.io.Console;
 
 @Controller
 public class IndexController {
+
 
     @GetMapping("/")
     public String index(){
