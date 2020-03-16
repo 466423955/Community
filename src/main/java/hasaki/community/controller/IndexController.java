2cf14c4890678f48ea7694143ec5651f0cb3a3ea
QHasakiQ <466423955@qq.com>
2020-03-16 19:24:12 +0800

    temp

--- src/main/java/hasaki/community/controller/IndexController.java
+++ src/main/java/hasaki/community/controller/IndexController.java
@@ -1,23 +1,12 @@
 package hasaki.community.controller;
 
 import hasaki.community.dto.PaginationDTO;
-import hasaki.community.dto.QuestionDTO;
-import hasaki.community.mapper.QuestionMapper;
-import hasaki.community.mapper.UserMapper;
-import hasaki.community.model.Question;
-import hasaki.community.model.User;
-import hasaki.community.provider.GithubProvider;
 import hasaki.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.io.Console;
-import java.util.List;
 
 /**
  *
